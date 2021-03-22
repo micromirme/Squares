8cbc79a2efb5f4573294a02180aa9f1adc5e9965
Vitaliy Zakharov <zaharov.v@outlook.com>
2021-03-22 23:18:08 +0300

    Initial Commit

---
+++ Squares/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Squares
+//
+//  Created by Micromirme on 22.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
